a203e9f47b34270ecf05f4a5ec7f918dd14d7ce0
Sofie Jensen <sasjensen@outlook.com>
2025-11-17 21:12:33 +0100

    Rødsorte trær og 2-3-4-trær - md

--- src/Binærtrær/RødSort.java
+++ src/Binærtrær/RødSort.java
@@ -1,95 +1,95 @@
-package Binærtrær;
-// https://www.youtube.com/watch?v=yiM8VlIJwms&list=TLPQMTIxMTIwMjWzLGqajkUnqg&index=8
-// Rotasjon kompleksitet O(1) - samme antall operasjoner i en rotasjon uansett antall elementer
-// PSEUDO KODE
-/*
-    venstreRoter (T, x) {
-        y = x.right // sett y - xs høyrebarn
-        x.right = y.left // sett xs høyrbarn til å peke på ys vensstrebarn
-
-        if (y.left != T.nil) { // dersom ys venstre ikke er en nil-node?
-            y.left.p = x; // y's venstrebarns foreldrenode settes til x
-        }
-        y.p = x.p; // sett ys foreldrenode til å være xs foreldrenode
-        if (x.p == T.nil) { // dersom xs forelder er en nil.node
-            T.root = y; // set y til å være roten
-        } else if (x == x.p.left) { // x er sin egen forelders venstrebarn
-            x.p.left = y; // forelders venstrebarn blir nå y  istedenfor
-        } else {
-            x.p.right = y; // x var høyrebarnet, men nå erstattes den av y
-        }
-        y.left = x; // x er ys venstrebarn
-        x.p = y; // y er xs forelder - vi kobler både y til x og x til y
-    }
-
- */
-
-import java.awt.*;
-
-// NODEKLASSE - for hver node
-class Node {
-    int data;
-    // i rødsorte har hver node en forelder-peker - effektiviserer innlegging/fjerning/rotasjon
-    Node left, right, parent;
-    Color color;
-
-    // konstruktør
-    public Node(int data) {
-        this.data = data;
-        this.color = Color.RED; // fordi nyinnsatte noder er røde
-        this.left = this.right = this.parent = null; // hvorfor?
-    }
-}
-
-// RØDSORT TRE KLASSE
-public class RødSort {
-    private Node root;
-    private final Node NIL; // tom node, alltid sort
-    // vi "erstatter" bladnoder med NIL - vi slipper å ha nullPointer-sjekk
-    // hele treet har teknisk sett 1 felles NIL-node
-
-    // konstruktør
-    public Rødsort() {
-        NIL = new Node(0); // har "ingen" verdi
-        NIL.color = Color.BLACK;
-        root = NIL; // ingen noder enda - rot er NIL
-    }
-
-    // VENSTREROTASJON - tar inn X
-    private void venstreRoter(Node x) { // x = roten i subtreet
-        Node y = x.right; // tmp Y - peker på Xs høyrebarn
-        x.right = y.left; // Ys venstrebarn blir Xs høyrebarn - bytter om og beholde BST struktur
-        if(y.left != NIL) { // så lenge Ys venstre ikke er NIL
-            y.left.parent = x; // oppdater foreldrenoden til å være X
-        }
-        y.parent = x.parent; // Xs forelder blir Ys forelder
-        if (x.parent == NIL) { // dersom X er rotnoden (den har ingen forelder)
-            root = y; // y blir den nye roten
-        } else if (x == x.parent.left) { // ellers - X er et venstrebarn
-            x.parent.left = y; // sett Y til å være venstrebarn
-        } else { // ellers - X er et høyrebarn
-            x.parent.right = y; // sett Y til å være høyrebarnet (erstatt X)
-        }
-        y.left = x; // sett X til Ys venstrebarn
-        x.parent = y; // koble X til Y (foreldrenode
-    }
-
-    // HØYREROTASJON - tar inn Y, og .right blir .left
-    private void høyreRotasjon (Node y) {
-        Node x = y.left; // X er Ys venstrebarn
-        y.left = x.right; // Xs høyrebarn blir Ys venstrebarn
-        if (x.right != NIL) { // dersom Xs høyre ikke er NIL
-            x.right.parent = y; // oppdater foreldrenoden til Y
-        }
-        x.parent = y.parent; // sett Xs foreldrenode til å være Ys forelder
-        if (y.parent == NIL) { // om Ys forelder er NIL er den roten
-            root = x; // sett X til å være rot
-        } else if (y == y.parent.right) { // dersom Y er høyrebarn
-            y.parent.right = x; // endre det til X
-        } else { // dersom Y er venstrebarn
-            y.parent.left = x; // bytt venstrebarnet til X
-        }
-        x.right = y; // sett Y til Xs høyrebarn
-        y.parent = x; // X blir Ys foreldrenode
-    }
-}
+//package Binærtrær;
+//// https://www.youtube.com/watch?v=yiM8VlIJwms&list=TLPQMTIxMTIwMjWzLGqajkUnqg&index=8
+//// Rotasjon kompleksitet O(1) - samme antall operasjoner i en rotasjon uansett antall elementer
+//// PSEUDO KODE
+///*
+//    venstreRoter (T, x) {
+//        y = x.right // sett y - xs høyrebarn
+//        x.right = y.left // sett xs høyrbarn til å peke på ys vensstrebarn
+//
+//        if (y.left != T.nil) { // dersom ys venstre ikke er en nil-node?
+//            y.left.p = x; // y's venstrebarns foreldrenode settes til x
+//        }
+//        y.p = x.p; // sett ys foreldrenode til å være xs foreldrenode
+//        if (x.p == T.nil) { // dersom xs forelder er en nil.node
+//            T.root = y; // set y til å være roten
+//        } else if (x == x.p.left) { // x er sin egen forelders venstrebarn
+//            x.p.left = y; // forelders venstrebarn blir nå y  istedenfor
+//        } else {
+//            x.p.right = y; // x var høyrebarnet, men nå erstattes den av y
+//        }
+//        y.left = x; // x er ys venstrebarn
+//        x.p = y; // y er xs forelder - vi kobler både y til x og x til y
+//    }
+//
+// */
+//
+//import java.awt.*;
+//
+//// NODEKLASSE - for hver node
+//class Node {
+//    int data;
+//    // i rødsorte har hver node en forelder-peker - effektiviserer innlegging/fjerning/rotasjon
+//    Node left, right, parent;
+//    Color color;
+//
+//    // konstruktør
+//    public Node(int data) {
+//        this.data = data;
+//        this.color = Color.RED; // fordi nyinnsatte noder er røde
+//        this.left = this.right = this.parent = null; // hvorfor?
+//    }
+//}
+//
+//// RØDSORT TRE KLASSE
+//public class RødSort {
+//    private Node root;
+//    private final Node NIL; // tom node, alltid sort
+//    // vi "erstatter" bladnoder med NIL - vi slipper å ha nullPointer-sjekk
+//    // hele treet har teknisk sett 1 felles NIL-node
+//
+//    // konstruktør
+//    public Rødsort() {
+//        NIL = new Node(0); // har "ingen" verdi
+//        NIL.color = Color.BLACK;
+//        root = NIL; // ingen noder enda - rot er NIL
+//    }
+//
+//    // VENSTREROTASJON - tar inn X
+//    private void venstreRoter(Node x) { // x = roten i subtreet
+//        Node y = x.right; // tmp Y - peker på Xs høyrebarn
+//        x.right = y.left; // Ys venstrebarn blir Xs høyrebarn - bytter om og beholde BST struktur
+//        if(y.left != NIL) { // så lenge Ys venstre ikke er NIL
+//            y.left.parent = x; // oppdater foreldrenoden til å være X
+//        }
+//        y.parent = x.parent; // Xs forelder blir Ys forelder
+//        if (x.parent == NIL) { // dersom X er rotnoden (den har ingen forelder)
+//            root = y; // y blir den nye roten
+//        } else if (x == x.parent.left) { // ellers - X er et venstrebarn
+//            x.parent.left = y; // sett Y til å være venstrebarn
+//        } else { // ellers - X er et høyrebarn
+//            x.parent.right = y; // sett Y til å være høyrebarnet (erstatt X)
+//        }
+//        y.left = x; // sett X til Ys venstrebarn
+//        x.parent = y; // koble X til Y (foreldrenode
+//    }
+//
+//    // HØYREROTASJON - tar inn Y, og .right blir .left
+//    private void høyreRotasjon (Node y) {
+//        Node x = y.left; // X er Ys venstrebarn
+//        y.left = x.right; // Xs høyrebarn blir Ys venstrebarn
+//        if (x.right != NIL) { // dersom Xs høyre ikke er NIL
+//            x.right.parent = y; // oppdater foreldrenoden til Y
+//        }
+//        x.parent = y.parent; // sett Xs foreldrenode til å være Ys forelder
+//        if (y.parent == NIL) { // om Ys forelder er NIL er den roten
+//            root = x; // sett X til å være rot
+//        } else if (y == y.parent.right) { // dersom Y er høyrebarn
+//            y.parent.right = x; // endre det til X
+//        } else { // dersom Y er venstrebarn
+//            y.parent.left = x; // bytt venstrebarnet til X
+//        }
+//        x.right = y; // sett Y til Xs høyrebarn
+//        y.parent = x; // X blir Ys foreldrenode
+//    }
+//}
--- src/Binærtrær/rødsorte_trær.md
+++ src/Binærtrær/rødsorte_trær.md
@@ -1,15 +1,60 @@
 # Rødsorte trær
 Et selvbalanserende binærsøketre. 
 Det er ekvivalent med 2-3-4-trær mht. balansering, men altså et ekte binærtre.
-
 2-3-4-trær er litt krevende å programmere fordi vi må oppfylle en rekke krav, særlig fjerning er litt utfordrende.
 
-
-**Regler:**
+### Regler:
 * Hver node er enten rød eller sort
 * Rotnoden er sort - dette er standard
 * NIL-noder er bladnoder med nullverdi (avbildes ofte ikke), de er alltid sorte
 * En rød node kan aldri ha røde barn
 * Fra rot til bladnode skal det alltid være samme antall sorte noder
   * Vi kan derfor snakke om "sort høyde"
-  * Ettersom det aldri kan være 2 rød på rad, er hver vei fra rot til blad maks dobblet så lang som kortest vei 
+  * Ettersom det aldri kan være 2 rød på rad, er hver vei fra rot til blad maks dobblet så lang som kortest vei
+
+### Terminologi
+**X** - den nye noden/den vi "ser" på  
+**F** - forelder  
+**S** - søsken (deler foreldrenode)  
+**T** - tante (forelders søsken)  
+**B** - besteforelder  
+
+![img_16.png](img_16.png)
+
+## To røde - og rød tante -> fargeskifte
+Vi bytter farge på forelder, tante og besteforelder. Så må vi sjekke at alt er lovlig høyere opp i treet og at vi ikke har fått 2 røde ved besteforelder.
+Altså er det besteforelder som nå er vår "x" node som vi må vurdere om er lovlig.
+B blir rød (fra sort), og forelder og tante blir sorte, slik at X er lovlig som rød.
+
+![img_19.png](img_19.png)
+
+## To røde - ingen rød tante -> rotasjon og fargskifte
+Vi må rotere Besteforelder ned mot Tanten, slik at Foreldrenoden kommer opp.
+Vi bytter farge på F og B.
+
+Vi gjør venstrerotasjon dersom X er venstrebarn av et venstrebarn.
+Høyrerotasjon dersom X er høyrebarn av et høyrebarn (de ligger på linje, uten en knekk).
+
+**Høyrerotasjon:**
+![img_17.png](img_17.png)
+
+**Venstrerotasjon:**
+![img_18.png](img_18.png)
+
+**Venstrerotasjon, med fargeskifte:**
+![img_23.png](img_23.png)
+![img_24.png](img_24.png)
+
+## To røde og en knekk -> dobbel rotasjon og fargeskifte
+Dersom vi tenker på rotasjon som på klokka, går vi til høyre, fra 6 til 12, og gjør en dobbel høyrerotasjon - dette er strengt tatt en venstre, og så en høyrerotasjon.
+En dobbel venstrerotasjon, å gå først et steg til høyre og så et til venstre, blir som å la viseren gå bakover, fra 6 til 12 via 3.
+
+Først roterer vi så X kommer opp og F blir dens barn (på motsatt side av det X var for F).
+Så roterer vi B ned mot T slik at X kommer opp. Eventuelle barn som ikke lenger får plass som venstre/høyrebarn av X kobles på nærmeste node med plass (bevar str-forhold, BST).
+Ved dobbelt rotasjon er vi opptatt av de to nodene som skal roteres - X og F, og deres barn = totalt 5 noder.
+
+Et triks for å se underveis om rotasjonene er gjort korrekt - se at inorden ikke har endret seg!
+
+![img_20.png](img_20.png)
+![img_21.png](img_21.png)
+![img_22.png](img_22.png)
